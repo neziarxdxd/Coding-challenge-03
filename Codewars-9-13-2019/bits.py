fe2e464c8351a7f6bd55698dc30b73d1bea799ae
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2019-09-13 23:38:58 +0800

    done

--- Codewars-9-13-2019/bits.py
+++ Codewars-9-13-2019/bits.py
@@ -1,1 +1,11 @@
-print("Hello")
+import numpy
+
+x=[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
+size= len(x) // 8
+x=([(x[(0+(i*8)):(8+(i*8))]) for i in range(size)])[::-1]
+t= []
+for i in x:
+    t+=i
+print(t)
+
+
